5f92e57faa0487b14e3aff9fa6a8b8e9c584b2c7
Sterling Smith <smithsp@fusion.gat.com>
2018-07-11 14:53:57 -0700

    Added tests to omas recipe

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,6 +3,6 @@ if 'USER' not in os.environ:
     os.environ['USER'] = 'TEST_CONDA_USER'
 if 'HOME' not in os.environ:
     os.environ['HOME'] = '/tmp'
-import omas
+import omas.tests
 import unittest
-unittest.main(omas)
+unittest.main(omas.tests)
