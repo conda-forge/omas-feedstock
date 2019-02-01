bde27292530962389ac5323542a315153b29fd1d
Sterling Smith <smithsp@fusion.gat.com>
2019-02-01 06:42:19 -0800

    Minor change to retrigger build

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,7 +2,7 @@ import os
 if 'USER' not in os.environ:
     os.environ['USER'] = 'TEST_CONDA_USER'
 if 'HOME' not in os.environ:
-    os.environ['HOME'] = '/tmp'
+    os.environ['HOME'] = '/tmp/'
 import omas
 import unittest
 unittest.main(omas)
